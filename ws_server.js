a23c9ac38cc93180d123c622430400eb2ab147a0
Lee Dongeun <sixgen@gmail.com>
2024-06-29 01:48:09 +0900

    update info

--- ws_server.js
+++ ws_server.js
@@ -4,51 +4,50 @@ import { WebSocketServer } from 'ws';
 
 const httpServer = createServer((req, res) => {
 
-
     console.log('req url:', req.url)
     if (req.url == '/') {
-      let content2 = readFileSync('index.html');
-      let length2 = Buffer.byteLength(content2);
-      res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Content-Length': length2
-      });
-      res.end(content2);
-  
-    } else {
-      res.writeHead(404);
-      res.end('Not found');
-      return;
-    }
-  
-  
-  });
-  
-  
-  httpServer.listen(8080);
+        let content2 = readFileSync('index.html');
+        let length2 = Buffer.byteLength(content2);
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': length2
+        });
+        res.end(content2);
 
-const wss = new WebSocketServer({ server: httpServer});
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
+        return;
+    }
+
+
+});
+
+
+httpServer.listen(8080);
+
+const wss = new WebSocketServer({ server: httpServer });
 
 
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
-  
-    ws.on('message', function message(data , isBinary) {
-        if(isBinary){
-            console.log('byteLength', data.byteLength )
-            console.log(data.toString('hex') )
+
+    ws.on('message', function message(data, isBinary) {
+        if (isBinary) {
+            console.log('byteLength', data.byteLength)
+            console.log(data.toString('hex'))
             data[0] = 255
-            ws.send( data )
-        }else if( data == 'request_4bytes'){
+            ws.send(data)
+        } else if (data == 'request_4bytes') {
             let buffer4 = Buffer.alloc(4)
-            ws.send( buffer4)
-        }else{
+            ws.send(buffer4)
+        } else {
             console.log('received: %s', data);
             ws.send('echo: ' + data)
         }
     });
-  
-    ws.send('server is ready.');
-  });
+
+    ws.send('The WebSocket connection has been established.');
+});
 
 
